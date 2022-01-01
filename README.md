1eb724a6d6d7ac93e57bb30fefe0c7a329f1f97c
Azargas <t.musset@outlook.fr>
2022-01-01 15:51:56 +0100

    add getting started guide in readme.md, add index default route

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ECE-BigData-API
 API for BigData project.
+
+# How to use
+1. Clone the repo
+2. Connect to VPN
+3. Generate a keytab from EDGE-1 and move into project root folder (same location as `krb5.conf`) and rename it "adaltas.keytab" (replace the old one)
+4. Go to `src/main/resources/application.properties` and add your principal (same used by keytab) like the example
+5. In terminal : 
+   1. `docker build -t BigData-API`
+   2. `docker run -p 8080:8080 BigData-API:latest`
+6. Access via browser : `http://localhost:8080`, you should see a message like this : "Yay, the server is accessible from docker !"
+7. Use Postman to test all the routes :
+   1. `/user`
+   2. `/channel`
+   3. `/message`
--- src/main/java/fr/ece/bigdata/project/controller/AppController.java
+++ src/main/java/fr/ece/bigdata/project/controller/AppController.java
@@ -1,37 +1,13 @@
 package fr.ece.bigdata.project.controller;
 
-import fr.ece.bigdata.project.config.HBaseConfig;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-
 @RestController
 public class AppController {
-    private final HBaseConfig hBaseConfig;
-
-    @Autowired
-    public AppController(HBaseConfig hBaseConfig) {
-        this.hBaseConfig = hBaseConfig;
-    }
-
-    @GetMapping(value = "/test")
-    public String test() {
-        try {
-            Connection connection = hBaseConfig.get();
-            Admin admin = connection.getAdmin();
-            for (TableName t : admin.listTableNames())
-                System.err.println(t.getNameAsString());
-            return "Connection success";
-        }
-        catch (IOException io) {
-            System.err.println("Unable to get user access");
-            io.printStackTrace();
-        }
-        return "Connection failed";
+    @GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String index() {
+        return "Yay, the server is accessible from docker !";
     }
 }
